fc075c78f684e0d96912178fbc1870031c9eb165
Sean Marcia <sean@cs.uoregon.edu>
2014-06-04 22:07:09 -0400

    Slimmed down the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
-  resources :videos
-  resources :categories
+  resources :videos, only: [:index, :show]
+  resources :categories, only: [:show]
   match 'home', to: 'videos#index', via: [:get, :post]
 end
